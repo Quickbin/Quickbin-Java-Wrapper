b1e76b46c0e325bc20fd0450517b28ae822097cf
Mihou <69381903+ShindouMihou@users.noreply.github.com>
2021-05-17 12:13:47 +0800

    Allow optional language for /v2/edit.

--- src/main/java/pw/quickbin/main/api/QuickBin.java
+++ src/main/java/pw/quickbin/main/api/QuickBin.java
@@ -1,11 +1,51 @@
 package pw.quickbin.main.api;
 
+import pw.quickbin.main.exceptions.InvalidRequestException;
+import pw.quickbin.main.exceptions.RateLimitedException;
+
 import java.util.concurrent.CompletableFuture;
 
 public interface QuickBin {
 
+    /**
+     * Edits a bin if it exists and is also owned by your token.
+     * @throws InvalidRequestException when the request is invalid.
+     * @throws RateLimitedException when you are rate-limited (1,000 requests a day).
+     * @param bin the bin to edit.
+     * @param content the new contents of the bin.
+     * @param language the new language of the bin.
+     * @return CompletableFuture<Void>
+     */
     CompletableFuture<Void> editBin(String bin, String content, String language);
+
+    /**
+     * Edits a bin if it exists and is also owned by your token.
+     * @throws InvalidRequestException when the request is invalid.
+     * @throws RateLimitedException when you are rate-limited (1,000 requests a day).
+     * @param bin the bin to edit.
+     * @param content the new contents of the bin.
+     * @return CompletableFuture<Void>
+     */
+    CompletableFuture<Void> editBin(String bin, String content);
+
+
+    /**
+     * Deletes a bin if it exists and is also owned by your token.
+     * @throws InvalidRequestException when the request is invalid.
+     * @throws RateLimitedException when you are rate-limited (1,000 requests a day).
+     * @param bin the bin to delete.
+     * @return CompletableFuture<Void>
+     */
     CompletableFuture<Void> deleteBin(String bin);
+
+    /**
+     * Creates a new bin with your token and user-agent.
+     * @throws InvalidRequestException when the request is invalid.
+     * @throws RateLimitedException when you are rate-limited (1,000 requests a day).
+     * @param content the content of the bin.
+     * @param language the language of the bin.
+     * @return CompletableFuture<Bin>
+     */
     CompletableFuture<Bin> createBin(String content, String language);
 
 }
